60766ef70b265e950d65d1bb18adc0ed3806bad0
SebastianSki <s.j.skibinski@gmail.com>
2021-03-12 22:13:49 +0100

    less lines

--- 3 Liczby/3liczby.cs
+++ 3 Liczby/3liczby.cs
@@ -6,66 +6,34 @@ namespace _3_liczby
     {
         static void Main(string[] args)
         {
-            int a = 51;
-            int b = 225;
-            int c = 33;
+            int a = 36;
+            int b = 6;
+            int c = 36;
 
-            if (a > b)
-            {
+           
+            if(a > b){
                 if (a > c)
-                {
                     Console.WriteLine("liczba a ma największą wartość");
-                }
+                else if (a == c)
+                    Console.WriteLine("liczby a i c są równe i mają największą wartość");
                 else
-                {
-                    if (a == c)
-                    {
-                        Console.WriteLine("liczby a i c są równe i mają największą wartość");
-                    }
-                    else
-                    {
-                        Console.WriteLine("liczba c ma największą wartość");
-                    }
-                }
-            }
-            else
-            {
-                if (a == b)
-                {
-                    if (c > a)
-                    {
-                        Console.WriteLine("liczba c ma największą wartość");
-                    }
-                    else
-                    {
-                        if (c == a)
-                        {
-                            Console.WriteLine("liczby a, b i c są równe, wszystkie mają największą wartość");
-                        }
-                        else
-                        {
-                            Console.WriteLine("liczby a i b są równe i mają największą wartość ");
-                        }
-                    }
-                }
+                    Console.WriteLine("liczba c ma największą wartość");
+
+            }else if(a == b){
+                if(c > a)
+                    Console.WriteLine("liczba c ma największą wartość");
+                else if(c == a)
+                    Console.WriteLine("liczby a, b i c są równe, wszystkie mają największą wartość");
                 else
-                {
-                    if (b > c)
-                    {
-                        Console.WriteLine("liczba b ma największą wartość");
-                    }
-                    else
-                    {
-                        if (b == c)
-                        {
-                            Console.WriteLine("liczby b i c są równe i mają największą wartość");
-                        }
-                        else
-                        {
-                            Console.WriteLine("liczba c ma największą wartość");
-                        }
-                    }
-                }
+                    Console.WriteLine("liczby a i b są równe i mają największą wartość ");
+                
+            }else{
+                if (b > c)
+                    Console.WriteLine("liczba b ma największą wartość");
+                else if (b == c)
+                    Console.WriteLine("liczby b i c są równe i mają największą wartość");
+                else
+                    Console.WriteLine("liczba c ma największą wartość");
             }
         }
     }
